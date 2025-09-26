f64c510b0d05883388416c2e7ae66b8e70c8208e
MayanthaS <sapumalmayantha@gmail.com>
2025-09-26 15:22:24 +0530

    code the validate inputs,create new record ,new td for title nd body,new td for view,new td for delete

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 /*variabel*/
 var form = document.getElementById('add-frm');
+var ntitle = document.getElementById('n-title');
+var nbody = document.getElementById('n-body');
 
 
 //---Events---
 form.addEventListener('submit',addNote);
 
 //--Function---
+
+//add Note
 function addNote(e){
+    //stop initial behaviour
     e.preventDefault();
-    console.log('Hello');
+   
+    //validate inputs
+    if(ntitle.value == ''|| nbody.value == ''){
+        alert('please enter fill all feilds');
+    }
+    else{
+        //create a new note record
+        //New tr
+        var tr = document.createElement('tr');
+        tr.className = 'items';
+
+        //new td for title and body
+        
+        var td1 =document.createElement('td');
+        td1.appendChild(document.createTextNode(ntitle.value));
+        var span =document.createElement('span');
+        span.className ='note-body';
+        span.appendChild(document.createTextNode(nbody.value));
+        td1.appendChild(span);
+ 
+        //new td for view
+        var td2 =document.createElement('td');
+        td2.className ='btcellv';
+        var btn1 = document.createElement('button');
+        btn1.appendChild(document.createTextNode('view'));
+        btn1.setAttribute('id','vw');
+        td2.appendChild(btn1);
+
+        //new td for delete
+         var td3 =document.createElement('td');
+        td2.className ='btcelld';
+        var btn2 = document.createElement('button');
+        btn2.appendChild(document.createTextNode('Delete'));
+        btn2.setAttribute('id','del');
+        td3.appendChild(btn2);
+
+
+        console.log(td3);
+    }
 }
